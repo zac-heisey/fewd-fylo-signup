7624f0818d24ba42a49d9e013e6c5dc210bd3d66
Zac Heisey <zheisey@gmail.com>
2019-04-09 19:07:37 -0400

    alert confirmation on email submission

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,19 @@
 // Get email input field
 var emailInput = document.querySelector('#email');
+// Get submit button
+var submitBtn = document.querySelector('[type="submit"]');
 
 // Get user input and return an alert message
 function emailConfirmation(event) {
-  console.log(event.target);
+  // Bail if click is NOT on submit button
+  if (event.target !== submitBtn) return;
+  // Get user email address
+  var userEmail = emailInput.value;
+  // Show alert confirmation message
+  alert(`Your email address has been successfully submitted: ${userEmail}`);
+  // Prevent default
+  event.preventDefault();
 }
 
+// Listen for clicks on document
 document.addEventListener('click', emailConfirmation, false);
